d2cadc967a42a59c0574b5a17d86b7dbf994a619
Igor Koshelev <igor@cleverflow.ru>
2016-06-20 18:27:42 +0300

    (no message)

--- !!autoupdate.cmd
+++ !!autoupdate.cmd
@@ -1,2 +1,6 @@
 git pull
-!build.cmd
+taskkill /f /im Bwl.GitManager.exe
+taskkill /f /im Bwl.GitManager.exe
+tools\vs-build-all.exe -debug -release *
+cd release\Bwl Git Manager\bin\
+start Bwl.GitManager.exe
